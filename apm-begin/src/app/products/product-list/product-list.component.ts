a70c327d5ba0770d03b4bab5b8e3527c8c1834ee
Your Name <you@example.com>
2026-04-08 10:00:17 +0200

    step_6: Handling HTTP Errors with Observables: catch error, better way

--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { NgIf, NgFor, NgClass } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../product.service';
-import { Subscription, tap } from 'rxjs';
+import { catchError, EMPTY, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'pm-product-list',
@@ -28,13 +28,31 @@ export class ProductListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.sub = this.productService
       .getProducts()
-      .pipe(tap(() => console.log('In ProductListComponent ngOnInit')))
-      .subscribe((products) => {
-        ((this.products = products),
-          console.log('log wewnątrz subskrypcji', this.products)); //ten log wyświetli już załadowane produkty, ponieważ znajduje się wewnątrz subskrypcji, która jest wywoływana po otrzymaniu danych z serwera.
+      .pipe(
+        tap(() => console.log('In ProductListComponent ngOnInit')),
+        catchError(err => {
+          this.errorMessage = err;
+          return EMPTY;
+        })
+      ).subscribe(products => {
+          this.products = products;
+            console.log('log wewnątrz subskrypcji', this.products); //ten log wyświetli już załadowane produkty, ponieważ znajduje się wewnątrz subskrypcji, która jest wywoływana po otrzymaniu danych z serwera.
       });
     console.log('log poza subskrypcją', this.products); //ten log wyświetli pustą tablicę na początku pojawienia się strony, ponieważ subskrypcja jest asynchroniczna i produkty nie zostały jeszcze załadowane w momencie wyświetlania tego loga.
   }
+//Ta opcja wyżej jest bardziej poprawna, ponieważ pozwala na obsługę błędów bezpośrednio w strumieniu danych i zapewnia, że logowanie produktów odbywa się tylko po ich załadowaniu. Opcja z logowaniem wewnątrz subskrypcji jest bardziej odpowiednia, ponieważ daje pewność, że produkty zostały załadowane przed próbą ich logowania, podczas gdy logowanie poza subskrypcją może prowadzić do nieoczekiwanych wyników, takich jak logowanie pustej tablicy produktów.
+    // .subscribe({
+    // next: products => {
+    // this.products = products;
+    // console.log('log wewnątrz subskrypcji', this.products);
+    // },
+    // error: err => {
+    // this.errorMessage = err;
+    // }
+    // });
+    // console.log('log poza subskrypcją', this.products);
+    // }
+
   ngOnDestroy(): void {
     console.log('ProductListComponent destroyed');
     this.sub.unsubscribe();
