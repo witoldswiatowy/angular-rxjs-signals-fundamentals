27533d6394219e52d368819f4b222ff0c8d2d6ad
Your Name <you@example.com>
2026-04-08 09:46:54 +0200

    step_6: Handling HTTP Errors with Observables: catch error

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,23 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
 import { Product } from './product';
+import { ProductData } from './product-data';
+import { HttpErrorService } from '../utilities/http-error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  private productsUrl = 'api/products';
+  private productsUrl = 'api/productss';
   private productUrl = 'api/products/';
 
   // constructor(private http: HttpClient) { } //zamiast tego lepiej użyć inject() w polu, bo to jest bardziej elastyczne i pozwala na łatwiejsze testowanie, czyli: private http = inject(HttpClient);
 
   private http = inject(HttpClient);
+  private httpErrorService = inject(HttpErrorService);
 
   getProducts(): Observable<Product[]> {
-    return this.http
-      .get<Product[]>(this.productsUrl)
-      .pipe(tap(() => console.log('In http.get pipeline')));
+    return this.http.get<Product[]>(this.productsUrl)
+      .pipe(
+        tap(() => console.log('In http.get pipeline')),
+        // catchError(err => {
+        //   console.error('Error in getProducts: ', err);
+        //   return of(ProductData.products);
+        // })
+        catchError(err => this.handleError(err))
+      );
   }
 
   // getProduct(id: number) {
@@ -25,7 +34,7 @@ export class ProductService {
   //   return this.http.get<Product>(productUrl);
   // }
 
-  //   getProduct(id: number): Observable<Product> {
+  //   getProduct(id: number): Observable<Produc> {
   //   return this.http.get<Product>(this.productUrl + id);
   // }
 
@@ -35,5 +44,9 @@ export class ProductService {
       .pipe(
         tap(() => console.log(`In http.get pipeline for id=${id}`))
       );
+  }
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    const formattedMessage = this.httpErrorService.formatError(err);
+    return throwError(() => formattedMessage);
   }
 }
