befd943aabc384c0e9dab708d67abcff35e52166
Your Name <you@example.com>
2026-04-08 11:26:23 +0200

    step_7: Getting Related Data: switchMap, concatMap and mergeMap

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, Observable, of, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, of, switchMap, tap, throwError } from 'rxjs';
 import { Product } from './product';
 import { ProductData } from './product-data';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root',
@@ -16,6 +18,7 @@ export class ProductService {
 
   private http = inject(HttpClient);
   private httpErrorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
@@ -43,8 +46,25 @@ export class ProductService {
       .get<Product>(this.productUrl + id)
       .pipe(
         tap(() => console.log(`In http.get pipeline for id=${id}`)),
+        switchMap(product => this.getProductWithReviews(product)),
+        //uzyto switchMap, bo przy wyborze innego produktu, chcemy anulować poprzednie żądanie recenzji i rozpocząć nowe dla nowego produktu. Nie musimy czekać aż załaduje się poprzedni produkt, żeby rozpocząć ładowanie recenzji dla nowego produktu. switchMap automatycznie anuluje poprzednie żądanie i rozpoczyna nowe, co jest idealne w tym przypadku.
+        
+        //dwie inne mapy, to concatMap i mergeMap, ale one nie anulują poprzedniego żądania, więc mogą prowadzić do sytuacji, gdzie mamy wiele żądań recenzji w toku, co może być niepożądane.
+        //concatMap: zachowuje kolejność żądań, ale nie anuluje poprzednich, więc jeśli użytkownik szybko wybiera różne produkty, może prowadzić do wielu żądań recenzji w toku. Robi pokolei żądania, więc jeśli użytkownik szybko wybiera różne produkty, może prowadzić do opóźnień w ładowaniu recenzji dla nowych produktów, ponieważ musi czekać aż poprzednie żądanie się zakończy.
+        //mergeMap: nie zachowuje kolejności i nie anuluje poprzednich, więc może prowadzić do sytuacji, gdzie recenzje dla różnych produktów są mieszane, co może być mylące dla użytkownika. Robi równolegle żądania, więc jeśli użytkownik szybko wybiera różne produkty, może prowadzić do wielu żądań recenzji w toku i mieszania wyników.
         catchError(err => this.handleError(err))
       );
+  }
+
+  getProductWithReviews(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+      .pipe(
+        map(reviews => ({ ...product, reviews }) as Product)
+      );
+    } else {
+      return of(product);
+    }
   }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
