f1a93a34266f79f1566a73906e09dce6d9568c48
Your Name <you@example.com>
2026-04-08 18:15:13 +0200

    remove to much comment

--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -20,46 +20,14 @@ export class ProductListComponent {
 
   // Products
   readonly products$ = this.productService.products$.pipe(
-    // tap(() => console.log('In ProductListComponent ngOnInit')),
     catchError((err) => {
       this.errorMessage = err;
       return EMPTY;
     }),
   );
-  // products: Product[] = [];
 
   // Selected product id to highlight the entry
-  // selectedProductId: number = 0;
   readonly selectedProductId$ = this.productService.productSelected$;
-
-  //zmieniliśmy na sposób deklaratywny, który jest bardziej elastyczny i łatwiejszy do testowania, ponieważ products$ jest strumieniem Observable,
-  //   ngOnInit(): void {
-  //     this.sub = this.productService.products$
-  //       .pipe(
-  //         tap(() => console.log('In ProductListComponent ngOnInit')),
-  //         catchError(err => {
-  //           this.errorMessage = err;
-  //           return EMPTY;
-  //         })
-  //       ).
-  //       subscribe(products => {
-  //           this.products = products;
-  //             console.log('log wewnątrz subskrypcji', this.products); //ten log wyświetli już załadowane produkty, ponieważ znajduje się wewnątrz subskrypcji, która jest wywoływana po otrzymaniu danych z serwera.
-  //       });
-  //     console.log('log poza subskrypcją', this.products); //ten log wyświetli pustą tablicę na początku pojawienia się strony, ponieważ subskrypcja jest asynchroniczna i produkty nie zostały jeszcze załadowane w momencie wyświetlania tego loga.
-  //   }
-  // //Ta opcja wyżej jest bardziej poprawna, ponieważ pozwala na obsługę błędów bezpośrednio w strumieniu danych i zapewnia, że logowanie produktów odbywa się tylko po ich załadowaniu. Opcja z logowaniem wewnątrz subskrypcji jest bardziej odpowiednia, ponieważ daje pewność, że produkty zostały załadowane przed próbą ich logowania, podczas gdy logowanie poza subskrypcją może prowadzić do nieoczekiwanych wyników, takich jak logowanie pustej tablicy produktów.
-  //     // .subscribe({
-  //     // next: products => {
-  //     // this.products = products;
-  //     // console.log('log wewnątrz subskrypcji', this.products);
-  //     // },
-  //     // error: err => {
-  //     // this.errorMessage = err;
-  //     // }
-  //     // });
-  //     // console.log('log poza subskrypcją', this.products);
-  //     // }
 
   onSelected(productId: number): void {
     this.productService.productSelected(productId);
--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,8 +1,17 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, map, Observable, of, shareReplay, switchMap, tap, throwError } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  map,
+  Observable,
+  of,
+  shareReplay,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
 import { Product } from './product';
-import { ProductData } from './product-data';
 import { HttpErrorService } from '../utilities/http-error.service';
 import { ReviewService } from '../reviews/review.service';
 import { Review } from '../reviews/review';
@@ -14,63 +23,30 @@ export class ProductService {
   private productsUrl = 'api/products';
   private productUrl = 'api/products/';
 
-  // constructor(private http: HttpClient) { } //zamiast tego lepiej użyć inject() w polu, bo to jest bardziej elastyczne i pozwala na łatwiejsze testowanie, czyli: private http = inject(HttpClient);
-
   private http = inject(HttpClient);
   private httpErrorService = inject(HttpErrorService);
   private reviewService = inject(ReviewService);
 
-  private productSelectedSubject = new BehaviorSubject<number | undefined>(undefined);
+  private productSelectedSubject = new BehaviorSubject<number | undefined>(
+    undefined,
+  );
   readonly productSelected$ = this.productSelectedSubject.asObservable();
 
-  readonly products$ = this.http.get<Product[]>(this.productsUrl)
-      .pipe(
-        tap(p => console.log(JSON.stringify(p))),
-        shareReplay(1), //dodajemy shareReplay() do strumienia products$, aby zapewnić, że dane są buforowane i udostępniane wszystkim subskrybentom, co pozwala uniknąć wielokrotnych żądań HTTP i poprawia wydajność aplikacji.
-        tap(() => console.log('After shareReplay in ProductService products$ pipeline')),
-        catchError(err => this.handleError(err))
-      );
-      //jest to sposób deklaratywny, który jest bardziej elastyczny i łatwiejszy do testowania, ponieważ products$ jest strumieniem Observable, 
-      // który można subskrybować w różnych miejscach aplikacji, a jego wartość będzie aktualizowana automatycznie, gdy dane zostaną pobrane. 
-
-
-      //jest to sposób proceduralny, który jest mniej elastyczny i trudniejszy do testowania, ponieważ metoda getProducts() musi być wywołana, 
-      // aby pobrać produkty, a to może prowadzić do problemów z synchronizacją i zarządzaniem stanem. W przeciwieństwie do tego, 
-      // użycie strumienia Observable pozwala na łatwiejsze zarządzanie asynchronicznością i lepszą integrację z innymi strumieniami danych w aplikacji.
-  // getProducts(): Observable<Product[]> {
-  //   return this.http.get<Product[]>(this.productsUrl)
-  //     .pipe(
-  //       tap(() => console.log('In http.get pipeline')),
-  //       // catchError(err => {
-  //       //   console.error('Error in getProducts: ', err);
-  //       //   return of(ProductData.products);
-  //       // })
-  //       catchError(err => this.handleError(err))
-  //     );
-  // }
-
-  // getProduct(id: number) {
-  //   const productUrl = this.productsUrl + '/' + id;
-  //   return this.http.get<Product>(productUrl);
-  // }
-
-  //   getProduct(id: number): Observable<Produc> {
-  //   return this.http.get<Product>(this.productUrl + id);
-  // }
+  readonly products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    tap((p) => console.log(JSON.stringify(p))),
+    shareReplay(1),
+    tap(() =>
+      console.log('After shareReplay in ProductService products$ pipeline'),
+    ),
+    catchError((err) => this.handleError(err)),
+  );
 
   getProduct(id: number): Observable<Product> {
-    return this.http
-      .get<Product>(this.productUrl + id)
-      .pipe(
-        tap(() => console.log(`In http.get pipeline for id=${id}`)),
-        switchMap(product => this.getProductWithReviews(product)),
-        //uzyto switchMap, bo przy wyborze innego produktu, chcemy anulować poprzednie żądanie recenzji i rozpocząć nowe dla nowego produktu. Nie musimy czekać aż załaduje się poprzedni produkt, żeby rozpocząć ładowanie recenzji dla nowego produktu. switchMap automatycznie anuluje poprzednie żądanie i rozpoczyna nowe, co jest idealne w tym przypadku.
-        
-        //dwie inne mapy, to concatMap i mergeMap, ale one nie anulują poprzedniego żądania, więc mogą prowadzić do sytuacji, gdzie mamy wiele żądań recenzji w toku, co może być niepożądane.
-        //concatMap: zachowuje kolejność żądań, ale nie anuluje poprzednich, więc jeśli użytkownik szybko wybiera różne produkty, może prowadzić do wielu żądań recenzji w toku. Robi pokolei żądania, więc jeśli użytkownik szybko wybiera różne produkty, może prowadzić do opóźnień w ładowaniu recenzji dla nowych produktów, ponieważ musi czekać aż poprzednie żądanie się zakończy.
-        //mergeMap: nie zachowuje kolejności i nie anuluje poprzednich, więc może prowadzić do sytuacji, gdzie recenzje dla różnych produktów są mieszane, co może być mylące dla użytkownika. Robi równolegle żądania, więc jeśli użytkownik szybko wybiera różne produkty, może prowadzić do wielu żądań recenzji w toku i mieszania wyników.
-        catchError(err => this.handleError(err))
-      );
+    return this.http.get<Product>(this.productUrl + id).pipe(
+      tap(() => console.log(`In http.get pipeline for id=${id}`)),
+      switchMap((product) => this.getProductWithReviews(product)),
+      catchError((err) => this.handleError(err)),
+    );
   }
 
   productSelected(selectedProductId: number): void {
@@ -79,10 +55,9 @@ export class ProductService {
 
   getProductWithReviews(product: Product): Observable<Product> {
     if (product.hasReviews) {
-      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
-      .pipe(
-        map(reviews => ({ ...product, reviews }) as Product)
-      );
+      return this.http
+        .get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(map((reviews) => ({ ...product, reviews }) as Product));
     } else {
       return of(product);
     }
