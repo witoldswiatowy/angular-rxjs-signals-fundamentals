acbe19f1c90827b4dbee7de111f86aa11e8342c0
Your Name <you@example.com>
2026-04-09 13:32:31 +0200

    step_11: Using Signals to Build a Shopping Cart Feature: Declaring a Computed Signal for the Cart Count Badge

--- apm-begin/src/app/cart/cart.service.ts
+++ apm-begin/src/app/cart/cart.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, effect, signal } from '@angular/core';
+import { Injectable, computed, effect, signal } from '@angular/core';
 import { CartItem } from './cart';
 import { Product } from '../products/product';
 
@@ -7,6 +7,10 @@ import { Product } from '../products/product';
 })
 export class CartService {
   cartItems = signal<CartItem[]>([]);
+
+  cartCount = computed(() => this.cartItems()
+    .reduce((count, item) => count + item.quantity, 0)
+  );
 
   eLenght = effect(() =>
     console.log('Cart array length:', this.cartItems().length),
