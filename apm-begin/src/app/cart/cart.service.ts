3279754d01d2b692d4bad8c92acc60eede2cdffe
Your Name <you@example.com>
2026-04-09 13:40:03 +0200

    step_11: Using Signals to Build a Shopping Cart Feature: Declaring Computed Signals for the Totals

--- apm-begin/src/app/cart/cart.service.ts
+++ apm-begin/src/app/cart/cart.service.ts
@@ -6,11 +6,26 @@ import { Product } from '../products/product';
   providedIn: 'root',
 })
 export class CartService {
+// Manage state with signals
   cartItems = signal<CartItem[]>([]);
 
+  // Number of items in the cart
   cartCount = computed(() => this.cartItems()
-    .reduce((count, item) => count + item.quantity, 0)
+    .reduce((accQty, item) => accQty + item.quantity, 0)
   );
+
+  // Total up the extended price for each item
+  subTotal = computed(() => this.cartItems().reduce((accTotal, item) =>
+    accTotal + (item.quantity * item.product.price), 0));
+
+  // Delivery is free if spending more than $50
+  deliveryFee = computed<number>(() => this.subTotal() < 50 ? 5.99 : 0);
+
+  // Tax could be based on shipping address zip code
+  tax = computed(() => Math.round(this.subTotal() * 10.75) / 100);
+
+  // Total price
+  totalPrice = computed(() => this.subTotal() + this.deliveryFee() + this.tax());
 
   eLenght = effect(() =>
     console.log('Cart array length:', this.cartItems().length),
