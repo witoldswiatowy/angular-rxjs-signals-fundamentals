334a616dfe5344b861debe7ef35f35bcb3a5158d
Your Name <you@example.com>
2026-04-09 14:34:43 +0200

    step_11: Using Signals to Build a Shopping Cart Feature: Declaring a Computed Signal for the Extended Price

--- apm-begin/src/app/cart/cart-item/cart-item.component.ts
+++ apm-begin/src/app/cart/cart-item/cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, computed, inject, Input, signal } from '@angular/core';
 import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -13,22 +13,28 @@ import { CartService } from '../cart.service';
 })
 export class CartItemComponent {
 
-  @Input({ required: true }) cartItem!: CartItem;
+  // Use a setter to set the signal 
+  // when the item is passed in from the parent component
+  @Input({ required: true }) set cartItem(ci: CartItem) {
+    this.item.set(ci);
+  }
 
   private cartService = inject(CartService);
+
+  item = signal<CartItem>(undefined!);
 
   // Quantity available (hard-coded to 8)
   // Mapped to an array from 1-8
   qtyArr = [...Array(8).keys()].map(x => x + 1);
 
   // Calculate the extended price
-  exPrice = this.cartItem?.quantity * this.cartItem?.product.price;
+  exPrice = computed(() => this.item().quantity * this.item().product.price);
 
   onQuantitySelected(quantity: number): void {
-    this.cartService.updateQuantity(this.cartItem, Number(quantity));
+    this.cartService.updateQuantity(this.item(), Number(quantity));
   }
 
   removeFromCart(): void {
-    this.cartService.removeFromCart(this.cartItem);
+    this.cartService.removeFromCart(this.item());
   }
 }
